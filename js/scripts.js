3319886db1794d7e94534a7894ad7a598fdbe2b2
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-05-16 11:05:46 -0400

    Added pokemonList Variable
    I added the variable for 10 different Pokemon and their Variables. I removed training code done previously.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-alert('Hello World');
-let favoritFood = 'Cheese';
-document.write(favoritFood);
+let pokemonList = [
+    {name: "Bulbausar", height: "6", type: ["Grass", "Poison"]},
+    {name: "Charmander", height: ".6", type: "Fire"},
+    {name: "Squirtle", height: "0.5", type: "Water"},
+    {name: "Pidgey", height: "0.3", type: ["Flying","Normal"]},
+    {name: "Pikachu", height: "0.4", type: "Electric"},
+    {name: "Jigglypuff", height: "0.5", type: ["Fairy","normal"]},
+    {name: "Psyduck", height: "0.8", type: "Water"},
+    {name: "Magnemite", height: "0.3", type: ["Electric","Steel"]},
+    {name: "Horsea", height: "0.4", type: "Water"},
+    {name: "Togepi", height: "0.3", type: "Fairy"}
+  ]
+  

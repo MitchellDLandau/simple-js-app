1ecde7227b07019e7da15a6551167f465aae1d97
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-05-25 15:48:23 -0400

    Named Buttons for Pokemon
    I made each Pokemon be represented by buttons on the page so they can be interacted with later. I did this by adding a function to the IIME and accessing it using my forEach function outside.

--- js/scripts.js
+++ js/scripts.js
@@ -20,17 +20,35 @@ let pokemonList = [
     return pokemonList;
   }
 
+  function addListItem(pokemon) {
+    let ListOfPokemon = document.querySelector('.pokemon-list');  
+    let aPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    aPokemon.appendChild(button);
+    ListOfPokemon.appendChild(aPokemon);
+  }
+
   return {
     add: add,
-    getAll: getAll  //Keys to be able to call on a function to be performed.
+    getAll: getAll,
+    addListItem: addListItem  //Keys to be able to call on a function to be performed.
   };
 
 })(); //IIFE to be able to keep all information within safe from interference so it can be called uppon in the future.
 
+console.log(pokemonRepository.getAll());
 pokemonRepository.add({name: 'Loki Bean', height: '0.3', type: 'Dog'});
 
- pokemonRepository.getAll().forEach(function(pokemon) {  //function to write the array to the document using forEach and keys (getAll) to access the information in IIFE
-    document.write('<p>' + pokemon.name + ' who is ' + pokemon.height + ' meters tall' + '</p>');
+ pokemonRepository.getAll().forEach(function (pokemon) {  //function to write the array to the document using forEach and keys (getAll) to access the information in IIFE
+  pokemonRepository.addListItem(pokemon);
+
+ });
+
+
+
+/*    document.write('<p>' + pokemon.name + ' who is ' + pokemon.height + ' meters tall' + '</p>');
     document.write
     if (pokemon.height > 0.65) {
       document.write("<h4>" + "Huge Pokemon" + "</h4>")
@@ -41,7 +59,7 @@ pokemonRepository.add({name: 'Loki Bean', height: '0.3', type: 'Dog'});
       else if (pokemon.height < 0.45) {
       document.write("<h4>" + "Just a Little Guy" + "</h4>");
      }})
-
+*/
   
 /*
   Old Code I am keeping for reference and will delete before posting final project

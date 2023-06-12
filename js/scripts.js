51660e89061d8ac2b3f52946c6972d1e88e73c57
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-06-12 12:00:55 -0400

    Wight and Abilities
    I added the weight and the abilities of the Pokemon to their modal. I also changed some colorings as well as added another Pokemon picture to the navigation bar.

--- js/scripts.js
+++ js/scripts.js
@@ -2,63 +2,36 @@ let pokemonRepository = (function () {
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  let modalContainer = document.querySelector('#modal-container');
+  // let modalContainer = document.querySelector('#modal-container'); not needed with bootstrap
     
-  function showModal (name, type, height, img) {
-    modalContainer.innerHTML = '';
-    let modal = document.createElement('div');
-    modal.classList.add('modal')
+  function showModal (name, type, height, weight, ability, img) {
 
-    let closeButtonElement = document.createElement ('button');
-    closeButtonElement.classList.add ('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
+    let modalBody = $(".modal-body");
+    let modalTitle = $(".modal-title");
 
-    let pokeName = document.createElement('h1');
-    pokeName.classList.add('poke-name');
-    pokeName.innerText = name;
+    modalTitle.empty();
+    modalBody.empty();
 
-    let pokeHeight = document.createElement('p');
-    pokeHeight.classList.add('poke-info');
-    pokeHeight.innerText = height;
+    let pokeName = $("<h1>" + name + "</h1>");
 
-    let pokeType = document.createElement('p');
-    pokeType.classList.add('poke-info');
-    pokeType.innerText = type;
+    let pokeHeight = $("<p>" + height + "</p>"); //bootstrap run modal. Personally written modal found below. 
 
-    let pokeImage = document.createElement('img');
-    pokeImage.classList.add('poke-image');
-    pokeImage.setAttribute('src', img);
-    // pokeImage.innerHTML = img;
+    let pokeWeight = $("<p>" + weight + "</p>");
 
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(pokeName);
-    modal.appendChild(pokeType)
-    modal.appendChild(pokeHeight);
-    modal.appendChild(pokeImage);
-    modalContainer.appendChild(modal);
-    
-    modalContainer.classList.add('is-visible');
+    let pokeType = $("<p>" + type + "</p>");
+
+    let pokeAbilities = $("<p>" + ability + "</p>");
+
+    let pokeImage = $('<img class="modal-image">');
+    pokeImage.attr("src", img);
+
+    modalTitle.append(pokeName);
+    modalBody.append(pokeType)
+    modalBody.append(pokeHeight);
+    modalBody.append(pokeWeight);
+    modalBody.append(pokeAbilities);
+    modalBody.append(pokeImage);
   }
-
-  function hideModal () {
-  let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-
-  modalContainer.addEventListener('click', (e) => {
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
 
   function add(pokemonAdd) {   //function to be able to add a pokemon to the array.
     pokemonList.push(pokemonAdd);
@@ -70,7 +43,7 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function showDetails(pokemon) {  //function that will print the pokemon information to the console log when a pokemon is clicked.
     loadDetails (pokemon).then(function(){
-      // console.log(pokemon)
+      console.log(pokemon)
       let name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
       let typeList = '';
       pokemon.types.forEach((type) => {
@@ -78,20 +51,32 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
         else typeList += ' and ' + type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1);
       });
       let height = 'Height: ' + pokemon.height + 'ft';
-      showModal(name, typeList, height, pokemon.imageUrl)//function that calls to the Modal that will show the information for the Pokemon.
+      let weight = 'Weight: ' + pokemon.weight + ' pounds';
+      let abilityList = '';
+      pokemon.abilities.forEach((ability) => {
+        if(ability.slot == 1) abilityList += 'Abilities: ' + ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1);
+        else abilityList += ' and ' + ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1);
+      });
+      showModal(name, typeList, height, weight, abilityList, pokemon.imageUrl)//function that calls to the Modal that will show the information for the Pokemon.
     });
   }
 
   function addListItem(pokemon) {  //crates buttons for every pokemon in the array. 
     let listOfPokemon = document.querySelector('.pokemon-list');  
     let aPokemon = document.createElement('li');
+    aPokemon.classList.add('list-group-item');
     let button = document.createElement('button');
     button.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     button.classList.add('button-class');
+    button.classList.add('btn');
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#poke-modal')
+
     button.addEventListener('click', function () {
         return showDetails(pokemon)
       } //sends the pokemon as its parameter to the showDetails function to return information to the console and make the modal.
       );
+    $('ul').addClass('list-group');
     aPokemon.appendChild(button);
     listOfPokemon.appendChild(aPokemon);
   }
@@ -119,7 +104,9 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     }).then(function (details) {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
+      item.weight = details.weight;
       item.types = details.types;
+      item.abilities = details.abilities;
   //    showModal(item); //added to call on function with let variables
     }).catch(function(e) {
       console.log(e);
@@ -141,6 +128,42 @@ pokemonRepository.loadList().then(function(){   //loads pokemon from the externa
  });
 });
 
+// $('ul').addClass('list-group')
 
 
 
+// modalContainer.innerHTML = '';
+//     let modal = document.createElement('div');
+//     modal.classList.add('modal')
+    
+//     let closeButtonElement = document.createElement ('button');
+//     closeButtonElement.classList.add ('modal-close');
+//     closeButtonElement.innerText = 'Close';
+//     closeButtonElement.addEventListener('click', hideModal);
+
+//     let pokeName = document.createElement('h1');
+//     pokeName.classList.add('poke-name');
+//     pokeName.innerText = name;
+
+//     let pokeHeight = document.createElement('p');
+//     pokeHeight.classList.add('poke-info');
+//     pokeHeight.innerText = height;
+
+//     let pokeType = document.createElement('p');
+//     pokeType.classList.add('poke-info');
+//     pokeType.innerText = type;
+
+//     let pokeImage = document.createElement('img');
+//     pokeImage.classList.add('poke-image');
+//     pokeImage.setAttribute('src', img);
+//     // pokeImage.innerHTML = img;
+
+//     modal.appendChild(closeButtonElement);
+//     modal.appendChild(pokeName);
+//     modal.appendChild(pokeType)
+//     modal.appendChild(pokeHeight);
+//     modal.appendChild(pokeImage);
+//     modalContainer.appendChild(modal);
+    
+//     modalContainer.classList.add('is-visible');
+//   }
